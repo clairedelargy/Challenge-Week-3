060e66066764c7673509646a24b6fe2e4668a4df
Claire DeLargy <clairedelargy@gmail.com>
2021-11-24 00:06:14 -0600

    added other files

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function myFunction() {
+
+  let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
+
+  var length = Number(prompt("How many characters would you like your password to be?"));
+  
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+
+var uppers = confirm("Would you like to use uppercase letters?");
+var lowers = confirm("Would you like to use lowercase letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?");
+
+while (!uppers && !lowers && !numbers && !symbols) {
+  alert("You must select at least one character type!");
+  uppers = confirm("Would you like to use uppercase letters?");
+  lowers = confirm("Would you like to use lowercase letters?");
+  numbers = confirm("Would you like to use numbers?");
+  symbols = confirm("Would you like to use special characters?");
+}
+
+  for (let i = 0; i < 8; i++) {
+      let generate = letters[Math.floor(Math.random() * 64)];
+      password += generate;
+
+  }
+
+
+  document.getElementById("password").innerHTML = password;
+}
